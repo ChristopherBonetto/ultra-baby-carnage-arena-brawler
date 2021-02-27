8abdb0a776f0fbcea2310a51a0f3319982cddf89
Patrick Coviello <cp0499@gmail.com>
2021-02-27 13:30:09 +0100

    Finalized wave manager in c++.

--- Source/UltraBabyCarnage/UBCWaveManager.cpp
+++ Source/UltraBabyCarnage/UBCWaveManager.cpp
@@ -1,64 +1,111 @@
 
 #include "UBCWaveManager.h"
+#include <iostream>
+#include <string>
 
-// Sets default values
 AUBCWaveManager::AUBCWaveManager()
 {
+	// Binds the delegates to the events.
+
 	waveStart.AddDynamic(this, &AUBCWaveManager::OnWaveStart);
 	roundStart.AddDynamic(this, &AUBCWaveManager::OnRoundStart);
 	roundUpdate.AddDynamic(this, &AUBCWaveManager::OnRoundUpdate);
 	roundDone.AddDynamic(this, &AUBCWaveManager::OnRoundDone);
 	waveDone.AddDynamic(this, &AUBCWaveManager::OnWaveDone);
-	enemySpawn.AddDynamic(this, &AUBCWaveManager::OnEnemySpawn);
 	enemySpawned.AddDynamic(this, &AUBCWaveManager::OnEnemySpawned);
 	anyEnemyDeath.AddDynamic(this, &AUBCWaveManager::OnAnyEnemyDeath);
 }
 
 void AUBCWaveManager::BeginPlay()
 {
-	waveStart.Broadcast();
+	if (wave->IsValidLowLevel())
+	{
+		// Starts the wave.
+		waveStart.Broadcast();
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(0, 5.f, FColor::Red, TEXT("The Wave Manager doesn't have a wave asset."));
+	}
 }
 
 void AUBCWaveManager::UpdateRound()
 {
-	roundUpdate.Broadcast();
+	// Ticks the round.
+	roundUpdate.Broadcast(GetWorld()->DeltaTimeSeconds);
 }
 
 void AUBCWaveManager::StartRound()
 {
+	// Starts the round.
 	roundStart.Broadcast();
-}
-
-void AUBCWaveManager::SpawnEnemies()
-{
-	enemySpawn.Broadcast();
 }
 
 void AUBCWaveManager::SpawnEnemy()
 {
+	// Sorts the spawn points.
 	SortSpawnPointsByDistance();
 
+	// Finds the spawn location of the enemy.
 	FTransform transform(FRotator::ZeroRotator, GetSpawnLocation());
 
 	FActorSpawnParameters spawnParams;
 	spawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
 	
-	AActor* enemy = GetWorld()->SpawnActor(*wave->GetEnemyClass(), &transform, spawnParams);
+	// Spawns the enemy.
+	AUBCCharacter* enemy = (AUBCCharacter*)GetWorld()->SpawnActor(*wave->GetEnemyClass(), &transform, spawnParams);
+
+	// Increase the counters.
 
 	currentEnemyCount++;
 	spawnedEnemyCount++;
+
+	// Binds to the death event of the enemy.
+	enemy->death.AddDynamic(this, &AUBCWaveManager::OnEnemyDeath);
+
+	// Invokes the enemy spawned event.
+	enemySpawned.Broadcast(enemy);
+}
+
+void AUBCWaveManager::SpawnEnemies()
+{
+	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, TEXT("Spawn Enemies."));
+	for (int i = 1; i <= enemyBatchSize; i++)
+	{
+		// Spawns an enemy.
+		SpawnEnemy();
+
+		// If enough enemies have been spawned the spawning ends.
+		if (spawnedEnemyCount >= roundEnemyCount)
+		{
+			GetWorldTimerManager().ClearTimer(enemySpawnTimer);
+			break;
+		}
+		// Else if the current count of active enemies is greater than the maximum amount of active enemies, 
+		// the spawning pauses until an enemy is killed.
+		else if (currentEnemyCount >= maxActiveEnemiesCount)
+		{
+			spawnHalted = true;
+			GetWorldTimerManager().ClearTimer(enemySpawnTimer);
+			break;
+		}
+	}
 }
 
 FVector AUBCWaveManager::GetSpawnLocation() const
 {
-	int size = FMath::Min(GetCutoffLength(), FMath::Min(maxActiveEnemySpawnersCount, enemySpawnPoints.Num()));
+	// Retrieves the minimum length of the array that respects the parameters.
+	int size = FMath::Min(GetCutoffLength(), FMath::Min(maxActiveSpawnPoints, enemySpawnPoints.Num()));
+	// Returns a random spawn point inside this pool.
 	return enemySpawnPoints[FMath::RandRange(0, size - 1)];
 }
 
 void AUBCWaveManager::SortSpawnPointsByDistance()
 {
+	// Retrieves the position of the player in the world.
 	FVector playerPosition = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation();
 
+	// Bubble sorts the spawn points using the distance from the player as the comparator.
 	for (int i = 0; i < enemySpawnPoints.Num(); i++)
 	{
 		for (int j = i; j < enemySpawnPoints.Num() - 1; j++)
@@ -78,97 +125,119 @@ void AUBCWaveManager::SortSpawnPointsByDistance()
 
 int AUBCWaveManager::GetCutoffLength() const
 {
+	// Retrieves the position of the player in the world.
 	FVector playerPosition = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation();
 
+	// Retrieves the size of the array.
 	int size = enemySpawnPoints.Num();
 
 	for (int i = 0; i < size; i++)
 	{
+		// If the spawn point is further than the cutoff distance, the size of the array before this element is returned.
 		if (FVector::Distance(enemySpawnPoints[i], playerPosition) > spawnCutoffDistance)
 		{
 			return i;
 		}
 	}
 
+	// All the spawn points respect the cutoff distance.
 	return size;
-}
-
-void AUBCWaveManager::AddSpawnPoints(TArray<FVector>& newSpawnPoints)
-{
-	enemySpawnPoints.Append(newSpawnPoints);
-}
-
-void AUBCWaveManager::AddSpawnPoint(FVector newSpawnPoint)
-{
-	enemySpawnPoints.Add(newSpawnPoint);
 }
 
 void AUBCWaveManager::OnWaveStart_Implementation() 
 {
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, TEXT("Wave started"));
+	// Retrieves the max active enemies count from the wave asset.
 	maxActiveEnemiesCount = wave->GetMaxActiveEnemiesCount();
+	// Starts the round.
 	roundStart.Broadcast();
 }
 
 void AUBCWaveManager::OnRoundStart_Implementation() 
 {
+	// Sets the round as not done and spawn halting as false.
 	isRoundDone = spawnHalted = false;
 
-	roundEnemyCount = wave->GetRoundSpawnCount(currentRound) * wave->GetEnemyMultiplier();
+	// Retrieves the amount of enemies to spawn this round from the asset.
+	roundEnemyCount = wave->GetRoundSpawnCount(currentRound);
+	
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, FString::FromInt(roundEnemyCount) + " enemies should spawn");
 
+	// If the enemy count is greater than 0, the round is actually started, else it is ended.
 	if (roundEnemyCount > 0)
 	{
 		currentEnemyCount = spawnedEnemyCount = 0;
 		enemyBatchSize = wave->GetRoundBatchSize(currentRound);
 		GetWorldTimerManager().SetTimer(enemySpawnTimer, this, &AUBCWaveManager::SpawnEnemies, wave->GetRoundSpawnDelay(currentRound), true);
-		GetWorldTimerManager().SetTimer(roundUpdateTimer, this, &AUBCWaveManager::UpdateRound, 0.f, true);
+		GetWorldTimerManager().SetTimer(roundUpdateTimer, this, &AUBCWaveManager::UpdateRound, .1f, true);
 	}
+	else
+	{
+		roundDone.Broadcast();
+	}
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, TEXT("Round " + FString::FromInt(currentRound) + " started"));
 }
 
-void AUBCWaveManager::OnRoundUpdate_Implementation()
+void AUBCWaveManager::OnRoundUpdate_Implementation(float deltaTime)
 {
+	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, FString::SanitizeFloat(GetSpawnTimeRemaining()) + "s remain to next spawn.");
 }
 
 void AUBCWaveManager::OnRoundDone_Implementation() 
 {
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, "Round " + FString::FromInt(currentRound) + " completed");
+
+	// The round is set as done.
 	isRoundDone = true;
+
+	// The round count is increased.
 	currentRound++;
+
+	// The tick of the round is stopped.
 	GetWorldTimerManager().ClearTimer(roundUpdateTimer);
-	GetWorldTimerManager().SetTimer(roundStartTimer, this, &AUBCWaveManager::StartRound, wave->GetRoundDelay(), false);
+	
+	// If the wave should end, it is ended.
+	if (ShouldWaveEnd())
+	{
+		waveDone.Broadcast();
+	}
+	// Else the timer for the next round is started.
+	else
+	{
+		GetWorldTimerManager().SetTimer(roundStartTimer, this, &AUBCWaveManager::StartRound, wave->GetRoundDelay(), false);
+	}
 }
 
 void AUBCWaveManager::OnWaveDone_Implementation() 
 {
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, TEXT("Wave completed"));
+
+	// The wave is set as done.
+	isWaveDone = true;
 }
 
-void AUBCWaveManager::OnEnemySpawn_Implementation() 
+void AUBCWaveManager::OnEnemySpawned_Implementation(AUBCCharacter* enemy)
 {
-	for (int i = 1; i < enemyBatchSize; i++)
-	{
-		SpawnEnemy();
-
-		if (spawnedEnemyCount >= roundEnemyCount)
-		{
-			if (currentEnemyCount >= maxActiveEnemiesCount)
-			{
-				spawnHalted = true;
-			}
-			
-			GetWorldTimerManager().ClearTimer(enemySpawnTimer);
-			break;
-		}
-	}
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, TEXT("Enemy spawned."));
 }
 
-void AUBCWaveManager::OnEnemySpawned_Implementation(AActor* enemy) 
+void AUBCWaveManager::OnEnemyDeath_Implementation(AUBCCharacter* enemy)
 {
+	anyEnemyDeath.Broadcast(enemy);
 }
 
-void AUBCWaveManager::OnAnyEnemyDeath_Implementation(AActor* enemy) 
+void AUBCWaveManager::OnAnyEnemyDeath_Implementation(AUBCCharacter* enemy)
 {
-	if (--currentEnemyCount <= 0 && spawnedEnemyCount >= roundEnemyCount)
+	// The active enemy count is decreased.
+	currentEnemyCount--;
+
+	// If the round should end, it is ended.
+	if (ShouldRoundEnd())
 	{
 		roundDone.Broadcast();
 	}
+	// Else if the spawnings were paused, they are resumed.
 	else if (spawnHalted)
 	{
 		spawnHalted = false;
@@ -176,7 +245,57 @@ void AUBCWaveManager::OnAnyEnemyDeath_Implementation(AActor* enemy)
 	}
 }
 
-bool AUBCWaveManager::IsRoundDone() const
+bool AUBCWaveManager::ShouldRoundEnd() const
 {
-	return isRoundDone;
+	// If the active enemy count is less or equal to 0 and enough enemies have been spawned.
+	return currentEnemyCount <= 0 && 
+		   spawnedEnemyCount >= roundEnemyCount;
 }
+
+bool AUBCWaveManager::ShouldWaveEnd() const
+{
+	// If the maximum amount of rounds have been reached and is not 0.
+	return maxRoundCount != 0 && 
+		   currentRound > maxRoundCount;
+}
+
+float AUBCWaveManager::GetRoundStartTimeRemaining() const
+{
+	return GetWorldTimerManager().GetTimerRemaining(roundStartTimer);
+}
+
+void AUBCWaveManager::ForceRoundDone()
+{
+	// The round is set as done an the timers are stopped.
+	isRoundDone = true;
+	GetWorldTimerManager().ClearTimer(roundUpdateTimer);
+	GetWorldTimerManager().ClearTimer(enemySpawnTimer);
+}
+
+void AUBCWaveManager::ForceWaveDone()
+{
+	// If the round is not done, it is forced to end.
+	if (!isRoundDone)
+	{
+		ForceRoundDone();
+	}
+
+	// The wave is ended.
+	waveDone.Broadcast();
+}
+
+void AUBCWaveManager::AddSpawnPoint(FVector newSpawnPoint)
+{
+	enemySpawnPoints.Add(newSpawnPoint);
+}
+
+void AUBCWaveManager::AddSpawnPoints(TArray<FVector> newSpawnPoints)
+{
+	enemySpawnPoints.Append(newSpawnPoints);
+}
+
+float AUBCWaveManager::GetSpawnTimeRemaining() const
+{
+	return GetWorldTimerManager().GetTimerRemaining(enemySpawnTimer);
+}
+
--- Source/UltraBabyCarnage/UBCWaveManager.h
+++ Source/UltraBabyCarnage/UBCWaveManager.h
@@ -1,11 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "UBCCharacter.h"
 #include "UBCWaveAsset.h"
 #include "UBCWaveManager.generated.h"
+
+/**
+* Manages a wave of Ultra Baby Carnage.
+*/
 
 UCLASS()
 class ULTRABABYCARNAGE_API AUBCWaveManager : 
@@ -17,21 +21,33 @@ class ULTRABABYCARNAGE_API AUBCWaveManager :
 
 public:
 
+	// Delegate used when the wave starts.
+
 	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FWaveStart);
+
+	// Delegate used when the round starts.
 	
 	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FRoundStart);
-	
-	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FRoundUpdate);
-	
+
+	// Delegate used when the round updates.
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FRoundUpdate, float, deltaTime);
+
+	// Delegate used when the round ends.
+
 	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FRoundDone);
-	
+
+	// Delegate used when the wave ends.
+
 	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FWaveDone);
 
-	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FEnemySpawn);
-	
-	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEnemySpawned, AActor*, enemy);
-	
-	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAnyEnemyDeath, AActor*, enemy);
+	// Delegate used when the an enemy has been spawned.
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEnemySpawned, AUBCCharacter*, enemy);
+
+	// Delegate used when any one of the spawned enemies dies.
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAnyEnemyDeath, AUBCCharacter*, enemy);
 
 #pragma endregion
 
@@ -43,14 +59,36 @@ protected:
 
 #pragma region Wave: Round attributes
 
+	/**
+	* Indicates whether the round is finished.
+	*/
+
 	UPROPERTY(BlueprintReadWrite, Category = "Wave: Round")
 	bool isRoundDone;
 
+	/**
+	* Indicates whether the wave is finished.
+	*/
+
+	UPROPERTY(BlueprintReadWrite, Category = "Wave: Round")
+	bool isWaveDone;
+
+	/**
+	* The current round of the wave.
+	*/
+
 	UPROPERTY(BlueprintReadWrite, Category = "Wave: Round")
 	int currentRound = 1;
+	/**
+	* The timer that handles the start of the next round.
+	*/
 
 	UPROPERTY(BlueprintReadWrite, Category = "Wave: Round")
 	FTimerHandle roundStartTimer;
+
+	/**
+	* The timer that ticks the update of the round.
+	*/
 
 	UPROPERTY(BlueprintReadWrite, Category = "Wave: Round")
 	FTimerHandle roundUpdateTimer;
@@ -59,23 +97,51 @@ protected:
 
 #pragma region Wave: Spawn attributes
 
+	/**
+	* Indicates whether the spawning of enemies has been paused.
+	*/
+
 	UPROPERTY(BlueprintReadWrite, Category = "Wave: Spawn")
 	bool spawnHalted;
+
+	/**
+	* The count of enemies that should be spawned this round.
+	*/
 
 	UPROPERTY(BlueprintReadWrite, Category = "Wave: Spawn")
 	int roundEnemyCount;
 
+	/**
+	* The count of currently active spawned enemies.
+	*/
+
 	UPROPERTY(BlueprintReadWrite, Category = "Wave: Spawn")
 	int currentEnemyCount;
+
+	/**
+	* The total count of enemies spawned this round.
+	*/
 
 	UPROPERTY(BlueprintReadWrite, Category = "Wave: Spawn")
 	int spawnedEnemyCount;
 
+	/**
+	* The maximum number of enemies that can be active at the same time.
+	*/
+
 	UPROPERTY(BlueprintReadWrite, Category = "Wave: Spawn")
 	int maxActiveEnemiesCount;
 
+	/**
+	* The number of enemies that will be spawned at the same time.
+	*/
+
 	UPROPERTY(BlueprintReadWrite, Category = "Wave: Spawn")
 	int enemyBatchSize;
+
+	/**
+	* The timer that handles spawnings.
+	*/
 
 	UPROPERTY(BlueprintReadWrite, Category = "Wave: Spawn")
 	FTimerHandle enemySpawnTimer;
@@ -88,20 +154,47 @@ public:
 
 #pragma region Public attributes
 	
+#pragma region Wave: Round attributes
+
+	/**
+	* Maximum number of rounds (0 = endless).
+	*/
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wave: Round")
+	int maxRoundCount;
+
+#pragma endregion
+
 #pragma region Wave: Spawn attributes
+
+	/**
+	* The maximum distance at which a spawner can be before being forced disabled.
+	*/
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wave: Spawn")
 	float spawnCutoffDistance;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wave: Spawn")
-	int maxActiveEnemySpawnersCount;
+	/**
+	* The maximum number of spawners that can be active at the same time.
+	*/
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wave: Spawn")
+	int maxActiveSpawnPoints;
+
+	/**
+	* The positions of the spawners.
+	*/
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wave: Spawn", Meta = (MakeEditWidget = true))
 	TArray<FVector> enemySpawnPoints;
 
 #pragma endregion
 
 #pragma region Wave: Info attributes
+
+	/**
+	* The asset on which the manager will base to progress the wave.
+	*/
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wave: Info")
 	UUBCWaveAsset* wave;
@@ -110,28 +203,53 @@ public:
 
 #pragma region Delegate definitions
 
-	UPROPERTY(BlueprintAssignable)
+	/**
+	* Invoked when the wave starts.
+	*/
+
+	UPROPERTY(BlueprintCallable, BlueprintAssignable, Category = "Wave: Round")
 	FWaveStart waveStart;
-	
-	UPROPERTY(BlueprintAssignable)
+
+	/**
+	* Invoked when the round starts.
+	*/
+
+	UPROPERTY(BlueprintCallable, BlueprintAssignable, Category = "Wave: Round")
 	FRoundStart roundStart;
-	
-	UPROPERTY(BlueprintAssignable)
+
+	/**
+	* Invoked when the round should be updated.
+	*/
+
+	UPROPERTY(BlueprintCallable, BlueprintAssignable, Category = "Wave: Round")
 	FRoundUpdate roundUpdate;
-	
-	UPROPERTY(BlueprintAssignable)
+
+	/**
+	* Invoked when the round ends.
+	*/
+
+	UPROPERTY(BlueprintCallable, BlueprintAssignable, Category = "Wave: Round")
 	FRoundDone roundDone;
-	
-	UPROPERTY(BlueprintAssignable)
+
+	/**
+	* Invoked when the wave ends.
+	*/
+
+	UPROPERTY(BlueprintCallable, BlueprintAssignable, Category = "Wave: Round")
 	FWaveDone waveDone;
-	
-	UPROPERTY(BlueprintAssignable)
-	FEnemySpawn enemySpawn;
-	
-	UPROPERTY(BlueprintAssignable)
+
+	/**
+	* Invoked when an enemy has been spawned.
+	*/
+
+	UPROPERTY(BlueprintCallable, BlueprintAssignable, Category = "Wave: Spawn")
 	FEnemySpawned enemySpawned;
-	
-	UPROPERTY(BlueprintAssignable)
+
+	/**
+	* Invoked when any of the spawned enemies dies.
+	*/
+
+	UPROPERTY(BlueprintCallable, BlueprintAssignable, Category = "Wave: Spawn")
 	FAnyEnemyDeath anyEnemyDeath;
 
 #pragma endregion
@@ -155,61 +273,115 @@ protected:
 	
 #pragma region Protected methods
 
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UFUNCTION(BlueprintCallable)
+	/**
+	* Updates the current round.
+	*/
+
+	UFUNCTION(BlueprintCallable, Category = "Wave: Round")
 	virtual void UpdateRound();
 
-	UFUNCTION(BlueprintCallable)
-	void StartRound();
+	/**
+	* Starts a round.
+	*/
 
-	UFUNCTION(BlueprintCallable)
-	void SpawnEnemies();
+	UFUNCTION(BlueprintCallable, Category = "Wave: Round")
+	void StartRound();
 	
-	UFUNCTION(BlueprintCallable)
+	/**
+	* Spawns an enemy to any of the spawner.
+	*/
+
+	UFUNCTION(BlueprintCallable, Category = "Wave: Spawn")
 	void SpawnEnemy();
 
-	UFUNCTION(BlueprintCallable)
+	/**
+	* Spawns a batch of enemies.
+	*/
+
+	UFUNCTION(BlueprintCallable, Category = "Wave: Spawn")
+	void SpawnEnemies();
+
+	/**
+	* Retrieves a spawn location for an enemy. 
+	* The location will be one of the n (n = maxActiveSpawnPoints) nearest to the player and that respects the cutoff distance.
+	*/
+
+	UFUNCTION(BlueprintCallable, Category = "Wave: Spawn")
 	FVector GetSpawnLocation() const;
 
-	UFUNCTION(BlueprintCallable)
+	/**
+	* Sorts the spawn points by distance from the player.
+	*/
+
+	UFUNCTION(BlueprintCallable, Category = "Wave: Spawn")
 	void SortSpawnPointsByDistance();
 
-	UFUNCTION(BlueprintCallable)
-	int GetCutoffLength() const;
+	/**
+	* Returns the count of spawners that respect the cutoff distance (spawn points MUST be ordered).
+	*/
 
-	UFUNCTION(BlueprintCallable)
-	void AddSpawnPoints(TArray<FVector>& newSpawnPoints);
-	
-	UFUNCTION(BlueprintCallable)
-	void AddSpawnPoint(FVector newSpawnPoint);
+	UFUNCTION(BlueprintCallable, Category = "Wave: Spawn")
+	int GetCutoffLength() const;
 
 #pragma region Events implementations
 
-	UFUNCTION(BlueprintNativeEvent)
+	/**
+	* Called when the wave starts.
+	*/
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Wave: Round")
 	void OnWaveStart();
 
-	UFUNCTION(BlueprintNativeEvent)
+	/**
+	* Called when the round starts.
+	*/
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Wave: Round")
 	void OnRoundStart();
 
-	UFUNCTION(BlueprintNativeEvent)
-	void OnRoundUpdate();
+	/**
+	* Called when the round updates.
+	*/
 
-	UFUNCTION(BlueprintNativeEvent)
+	UFUNCTION(BlueprintNativeEvent, Category = "Wave: Round")
+	void OnRoundUpdate(float deltaTime);
+
+	/**
+	* Called when the round ends.
+	*/
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Wave: Round")
 	void OnRoundDone();
 
-	UFUNCTION(BlueprintNativeEvent)
+	/**
+	* Called when the wave ends.
+	*/
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Wave: Round")
 	void OnWaveDone();
 
-	UFUNCTION(BlueprintNativeEvent)
-	void OnEnemySpawn();
+	/**
+	* Called when an enemy is spawned.
+	*/
 
-	UFUNCTION(BlueprintNativeEvent)
-	void OnEnemySpawned(AActor* enemy);
+	UFUNCTION(BlueprintNativeEvent, Category = "Wave: Spawn")
+	void OnEnemySpawned(AUBCCharacter* enemy);
 
-	UFUNCTION(BlueprintNativeEvent)
-	void OnAnyEnemyDeath(AActor* enemy);
+	/**
+	* Called when a spawned enemy dies.
+	*/
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Wave: Spawn")
+	void OnEnemyDeath(AUBCCharacter* enemy);
+
+	/**
+	* Called when any of the spawned enemies die.
+	*/
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Wave: Spawn")
+	void OnAnyEnemyDeath(AUBCCharacter* enemy);
 
 #pragma endregion
 
@@ -223,8 +395,57 @@ public:
 	* Returns whether the round should end.
 	*/
 
-	UFUNCTION(BlueprintCallable)
-	bool IsRoundDone() const;
+	UFUNCTION(BlueprintCallable, Category = "Wave: Round")
+	bool ShouldRoundEnd() const;
+
+	/**
+	* Returns whether the wave should end.
+	*/
+
+	UFUNCTION(BlueprintCallable, Category = "Wave: Round")
+	bool ShouldWaveEnd() const;
+
+	/**
+	* Returns the amount of time in seconds that remains before the next round is started.
+	*/
+
+	UFUNCTION(BlueprintCallable, Category = "Wave: Round")
+	float GetRoundStartTimeRemaining() const;
+
+	/**
+	* Forces the round to end.
+	*/
+
+	UFUNCTION(BlueprintCallable, Category = "Wave: Round")
+	void ForceRoundDone();
+
+	/**
+	* Forces the wave to end.
+	*/
+
+	UFUNCTION(BlueprintCallable, Category = "Wave: Round")
+	void ForceWaveDone();
+
+	/**
+	* Adds a single spawn point to the spawn points of the wave.
+	*/
+
+	UFUNCTION(BlueprintCallable, Category = "Wave: Spawn")
+	void AddSpawnPoint(FVector newSpawnPoint);
+
+	/**
+	* Adds an array of spawn points to the spawn points of the wave.
+	*/
+
+	UFUNCTION(BlueprintCallable, Category = "Wave: Spawn")
+	void AddSpawnPoints(TArray<FVector> newSpawnPoints);
+
+	/**
+	* Returns the amount of time in seconds that remains before the next batch of enemies is spawned.
+	*/
+
+	UFUNCTION(BlueprintCallable, Category = "Wave: Spawn")
+	float GetSpawnTimeRemaining() const;
 
 #pragma endregion
 

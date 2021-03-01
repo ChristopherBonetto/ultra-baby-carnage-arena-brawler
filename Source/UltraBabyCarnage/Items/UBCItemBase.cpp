ee6565b1db33f2150ef13a95e8013e88817ba05a
Patrick Coviello <cp0499@gmail.com>
2021-03-01 12:25:29 +0100

    Created c++ item base class.

---
+++ Source/UltraBabyCarnage/Items/UBCItemBase.cpp
@@ -0,0 +1,2 @@
+
+#include "UBCItemBase.h"
---
+++ Source/UltraBabyCarnage/Items/UBCItemBase.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "UBCItemBase.generated.h"
+
+UCLASS()
+class ULTRABABYCARNAGE_API AUBCItemBase : 
+	public AActor
+{
+	GENERATED_BODY()
+
+};

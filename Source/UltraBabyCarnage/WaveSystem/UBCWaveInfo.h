18be925d406efc688fa0a9c53651dcc1230539b7
Patrick Coviello <cp0499@gmail.com>
2021-03-02 11:10:16 +0100

    Added item budget related variables to the wave info struct.

--- Source/UltraBabyCarnage/WaveSystem/UBCWaveInfo.h
+++ Source/UltraBabyCarnage/WaveSystem/UBCWaveInfo.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Curves/CurveFloat.h"
+#include "Engine/DataTable.h"
 #include "../UBCCharacter.h"
 #include "UBCWaveInfo.generated.h"
 
@@ -24,21 +25,21 @@ public:
 	*/
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float roundDelay;
+	float roundDelay = 5.f;
 
 	/**
 	* Value multiplied to the enemy spawn curve to decide how many enemies to spawn.
 	*/
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	int enemyMultiplier;
+	int enemyMultiplier = 1;
 
 	/**
 	* Maximum amount of enemies that can be active at the same time.
 	*/
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	int maxActiveEnemiesCount;
+	int maxActiveEnemiesCount = 10;
 
 	/**
 	* Curve that indicates how many enemies should be spawned per round.
@@ -74,6 +75,55 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UCurveFloat* enemyPowerCurve;
+
+	/**
+	* The base item budget of a round.
+	*/
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int baseBudget = 1;
+
+	/**
+	* The minimum item budget multiplier of a round.
+	*/
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int minBudgetMultiplier = 1;
+
+	/**
+	* The maximum item budget multiplier of a round.
+	*/
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int maxBudgetMultiplier = 10;
+
+	/**
+	* Data table that contains the items that will be used and their cost.
+	*/
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UDataTable* itemCostTable;
+
+	/**
+	* Curve that indicates the start index of the items that will be considered in the table.
+	*/
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UCurveFloat* itemIndexOffsetCurve;
+
+	/**
+	* Curve that indicates the indeces in the item table that will be considered per round.
+	*/
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UCurveFloat* maxItemIndexCurve;
+
+	/**
+	* Curve that indicates the index of the default item that will be used each round in the table.
+	*/
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UCurveFloat* defaultItemIndexCurve;
 
 	/**
 	* Class of the enemy that will be spawned.
